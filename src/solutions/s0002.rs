78905e4721bcf0743156ee86b186683c72bb1315
Vichayturen <1073931273@qq.com>
2025-03-09 04:07:56 +0800

    add 0002

--- src/solutions/s0002.rs
+++ src/solutions/s0002.rs
@@ -22,7 +22,30 @@ impl ListNode {
 
 impl Solution {
     pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        
+        let mut head = Box::new(ListNode::new(0));
+        let mut tail = &mut head;
+        let mut acc = 0;
+        let mut l1 = l1;
+        let mut l2 = l2;
+        while l1.is_some() || l2.is_some() {
+            let mut now = acc;
+            if let Some(node) = l1 {
+                now += node.val;
+                l1 = node.next;
+            }
+            if let Some(node) = l2 {
+                now += node.val;
+                l2 = node.next;
+            }
+            acc = now / 10;
+            tail.next = Some(Box::new(ListNode::new(now % 10)));
+            tail = tail.next.as_mut().unwrap();
+        }
+        if acc > 0 {
+            tail.next = Some(Box::new(ListNode::new(acc)));
+            tail = tail.next.as_mut().unwrap();
+        }
+        head.next
     }
 }
 
@@ -33,7 +56,7 @@ mod tests {
 
     #[test]
     fn test() {
-        let res = Solution::two_sum(Vec::from([2, 7, 4, 3]), 9);
-        println!("{:?}", res);
+        // let res = Solution::add_two_numbers(Vec::from([2, 7, 4, 3]), 9);
+        // println!("{:?}", res);
     }
 }
